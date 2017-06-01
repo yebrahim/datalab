24ef7e76bc6f17a3bd5f9e0fe0ba319f14d5a72d
Yasser Elsayed <yelsayed@google.com>
2017-06-01 10:19:42 -0700

    websocket polyfill

--- sources/web/datalab/static/websocket.js
+++ sources/web/datalab/static/websocket.js
@@ -1,9 +1,5 @@
 define(['util'], (util) => {
 
-  function shouldShimWebsockets() {
-    return location.host.toLowerCase().substr(-12) === '.appspot.com';
-  }
-
   // Override WebSocket
   (function() {
     if (!window.io) {
@@ -12,85 +8,119 @@ define(['util'], (util) => {
       return;
     }
 
-    function WebSocketShim(url) {
+    function WebSocketPolyfill(url) {
       var self = this;
-      this._url = url;
-      this.readyState = WebSocketShim.CLOSED;
+      self._url = url;
 
-      var socketUri = location.protocol + '//' + location.host + '/session';
-      var socketOptions = {
-        upgrade: false,
-        multiplex: false
-      };
+      // This uses a SocketUI object to polyfill the WebSocket functionality
+      if (window.useSocketIO) {
+        this._readyState = WebSocketPolyfill.CLOSED;
+        var socketUri = location.protocol + '//' + location.host + '/session';
+        var socketOptions = {
+          upgrade: false,
+          multiplex: false
+        };
 
-      function errorHandler() {
-        if (self.onerror) {
-          self.onerror({ target: self });
+        function errorHandler() {
+          if (self.onerror) {
+            self.onerror({ target: self });
+          }
+        }
+        var socket = io.connect(socketUri, socketOptions);
+        socket.on('connect', function() {
+          socket.emit('start', { url: url });
+        });
+        socket.on('disconnect', function() {
+          self._socket = null;
+          self._readyState = WebSocketPolyfill.CLOSED;
+          if (self.onclose) {
+            self.onclose({ target: self });
+          }
+        });
+        socket.on('open', function(msg) {
+          self._socket = socket;
+          self._readyState = WebSocketPolyfill.OPEN;
+          if (self.onopen) {
+            self.onopen({ target: self });
+          }
+        });
+        socket.on('close', function(msg) {
+          self._socket = null;
+          self._readyState = WebSocketPolyfill.CLOSED;
+          if (self.onclose) {
+            self.onclose({ target: self });
+          }
+        });
+        socket.on('data', function(msg) {
+          if (self.onmessage) {
+            self.onmessage({ target: self, data: msg.data });
+          }
+        });
+        socket.on('error', errorHandler);
+        socket.on('connect_error', errorHandler);
+        socket.on('reconnect_error', errorHandler);
+
+      } else {
+        // a thin shim around the native WebSocket object to catch its onerror
+        // and switch to SocketIO
+        self._ws = new window.nativeWebSocket(url)
+        self._ws.onopen = function() {
+          self.onopen.apply(self, arguments);
+        }
+        self._ws.onclose = function() {
+          self.onclose.apply(self, arguments);
+        }
+        self._ws.onmessage = function() {
+          self.onmessage.apply(self, arguments);
+        }
+        self._ws.onerror = function() {
+          util.debug.log('Native WebSocket failed. Replacing with socket.io');
+          // save this state so we only switch on failure the first time
+          window.useSocketIO = true;
         }
       }
-      var socket = io.connect(socketUri, socketOptions);
-      socket.on('connect', function() {
-        socket.emit('start', { url: url });
-      });
-      socket.on('disconnect', function() {
-        self._socket = null;
-        self.readyState = WebSocketShim.CLOSED;
-        if (self.onclose) {
-          self.onclose({ target: self });
-        }
-      });
-      socket.on('open', function(msg) {
-        self._socket = socket;
-        self.readyState = WebSocketShim.OPEN;
-        if (self.onopen) {
-          self.onopen({ target: self });
-        }
-      });
-      socket.on('close', function(msg) {
-        self._socket = null;
-        self.readyState = WebSocketShim.CLOSED;
-        if (self.onclose) {
-          self.onclose({ target: self });
-        }
-      });
-      socket.on('data', function(msg) {
-        if (self.onmessage) {
-          self.onmessage({ target: self, data: msg.data });
-        }
-      });
-      socket.on('error', errorHandler);
-      socket.on('connect_error', errorHandler);
-      socket.on('reconnect_error', errorHandler);
     }
-    WebSocketShim.prototype = {
+    WebSocketPolyfill.prototype = {
       onopen: null,
       onclose: null,
       onmessage: null,
       onerror: null,
-
-      send: function(data) {
-        if (this.readyState != WebSocketShim.OPEN) {
-          throw new Error('WebSocket is not yet opened');
+      get readyState() {
+        if (window.useSocketIO) {
+          return this._readyState;
+        } else {
+          return this._ws.readyState;
         }
-        this._socket.emit('data', { data: data });
       },
-
+      send: function(data) {
+        if (window.useSocketIO) {
+          if (this._readyState != WebSocketPolyfill.OPEN) {
+            throw new Error('WebSocket is not yet opened');
+          }
+          this._socket.emit('data', { data: data });
+        } else {
+          this._ws.send.apply(this._ws, arguments);
+        }
+      },
       close: function() {
-        if (this.readyState == WebSocketShim.OPEN) {
-          this.readyState = WebSocketShim.CLOSED;
+        if (window.useSocketIO) {
+          if (this._readystate == WebSocketPolyfill.open) {
+            this._readystate = WebSocketPolyfill.closed;
 
-          this._socket.emit('stop', { url: this._url });
-          this._socket.close();
+            this._socket.emit('stop', { url: this._url });
+            this._socket.close();
+          }
+        } else {
+          this._ws.close.apply(this._ws, arguments);
         }
       }
-    };
-    WebSocketShim.CLOSED = 0;
-    WebSocketShim.OPEN = 1;
-
-    if (shouldShimWebsockets()) {
-      util.debug.log('Replacing native websockets with socket.io');
-      window.nativeWebSocket = window.WebSocket;
-      window.WebSocket = WebSocketShim;
     }
+    WebSocketPolyfill.CONNECTING = 0;
+    WebSocketPolyfill.OPEN = 1;
+    WebSocketPolyfill.CLOSING = 2;
+    WebSocketPolyfill.CLOSED = 3;
+
+    window.nativeWebSocket = window.WebSocket;
+    window.WebSocket = WebSocketPolyfill;
   })();
 });
